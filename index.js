c2b6cc0e391ad30d514507ccb406f395417a7995
FrostyyPaws <thefrostyypaws@gmail.com>
2021-10-06 16:29:07 -0400

    add: run function for organization

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const fs = require('fs');
 const TEXT_FILE_TO_HASH = "myfakepassword.txt";
 function getStrFromFile(fileName) {
     return new Promise((resolve, reject) => {
-        fs.readFile(TEXT_FILE_TO_HASH, "utf-8", (err, data) => {
+        fs.readFile(fileName, "utf-8", (err, data) => {
             if (err) reject(err);
             resolve(data);
         })
     })
 };
 function createHashFromString(str) {
-    
+    let hash = "placeholder";
+    return hash
 };
+
+
+async function run(){ 
+    let str = await getStrFromFile(TEXT_FILE_TO_HASH);
+    let hash = createHashFromString(str);
+}
