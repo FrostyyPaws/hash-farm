45aad55c1b5c46d9f0beaf8e89a0c927dd9d2c6a
FrostyyPaws <thefrostyypaws@gmail.com>
2021-10-07 01:34:04 -0400

    add:hash algo init

--- generateHash.js
+++ generateHash.js
@@ -9,13 +9,34 @@ function getStrFromFile(fileName) {
     })
 };
 function createHashFromString(str) {
-    let binary = str.split('').map(c => c.charCodeAt(0).toString(2)).join(' ');
-    let hash = binary; //placeholder for now while testing and learning
-    return hash
+    const DIGITS_LIMIT = 38;
+    let seed = str.split('').map(c => c.charCodeAt(0)).join('');
+    let cycles = seed.length;
+    let repeatCycles = Number(seed[length-1]) + 2
+    console.log(cycles); 
+    console.log(seed);
+    for (let i = 0; i < cycles * repeatCycles; i++) {
+        seed = seed.split('')
+        let fertilizer = (Number(seed.splice(0,1)[0])+1);
+        seed = BigInt(seed.join(''));
+        seed *= BigInt(fertilizer);
+        seed = seed.toString();
+        if (i>cycles){ 
+            seed = seed.substr(0,DIGITS_LIMIT-1);
+        }
+        seed += fertilizer.toString();
+        if (seed.length < DIGITS_LIMIT) i--;
+
+    }
+    console.log(seed);
+    return seed
 };
-async function run(){ 
+async function run() {
     let str = await getStrFromFile(TEXT_FILE_TO_HASH);
     let hash = createHashFromString(str);
-    console.log(`Original:${str}  /  Hash:${hash}`)
+    //console.log(`Original:${str}  /  Hash:${hash}`)
 }
 run();
+//we need entropy, one-way mutations, and salt
+
+
