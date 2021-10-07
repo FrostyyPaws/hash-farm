f528cf974dccf77269b9717a2d7975d4f8a62c33
FrostyyPaws <thefrostyypaws@gmail.com>
2021-10-07 01:55:35 -0400

    add: pass string itself from commandline, instead of file ref to string

--- generateHash.js
+++ generateHash.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const TEXT_FILE_TO_HASH = process.argv[2];
+const TEXT_FILE_TO_HASH = "";
+const PASSED_STRING_TO_HASH = process.argv[2];
 function getStrFromFile(fileName) {
     return new Promise((resolve, reject) => {
         fs.readFile(fileName, "utf-8", (err, data) => {
@@ -12,29 +13,31 @@ function createHashFromString(str) {
     const DIGITS_LIMIT = 38;
     let seed = str.split('').map(c => c.charCodeAt(0)).join('');
     let cycles = seed.length;
-    let repeatCycles = Number(seed[length-1]) + 2
-    console.log(cycles); 
-    console.log(seed);
+    let repeatCycles = Number(seed[seed.length - 1]) + 2
     for (let i = 0; i < cycles * repeatCycles; i++) {
         seed = seed.split('')
-        let fertilizer = (Number(seed.splice(0,1)[0])+1);
+        let fertilizer = (Number(seed.splice(0, 1)[0]) + 1);
         seed = BigInt(seed.join(''));
         seed *= BigInt(fertilizer);
         seed = seed.toString();
-        if (i>cycles){ 
-            seed = seed.substr(0,DIGITS_LIMIT-1);
+        if (i > cycles) {
+            seed = seed.substr(0, DIGITS_LIMIT - 1);
         }
         seed += fertilizer.toString();
         if (seed.length < DIGITS_LIMIT) i--;
 
     }
-    console.log(seed);
     return seed
 };
 async function run() {
-    let str = await getStrFromFile(TEXT_FILE_TO_HASH);
+    let str;
+    if (TEXT_FILE_TO_HASH) {
+        str = await getStrFromFile(TEXT_FILE_TO_HASH);
+    } else {
+        str = PASSED_STRING_TO_HASH;
+    }
     let hash = createHashFromString(str);
-    //console.log(`Original:${str}  /  Hash:${hash}`)
+    console.log(`Hash:${hash} / Original:${str}`)
 }
 run();
 //we need entropy, one-way mutations, and salt
